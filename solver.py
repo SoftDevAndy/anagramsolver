f1e81d15e95a2b0020f45f54ffeee836bd08259e
AndyDev2013 <sweeneyandrew2013@gmail.com>
2016-03-13 00:48:25 +0000

    Fixed a bug and an oversight

--- solver.py
+++ solver.py
@@ -8,34 +8,6 @@ import collections
 import time
 
 
-def recursive(permuset, prefix, word):
-    i = 0
-    if len(word) <= 1:
-        permuset.add(prefix + word)
-    else:
-        while i < len(word):
-
-            x = i + 1
-
-            current = word[i:x]
-            before = word[0:i]
-            after = word[x:]
-            recursive(permuset, prefix + current, before + after)
-            i += 1
-
-
-def get_permuations(given_word):
-    permus = set()
-
-    print("\nPreprocessing")
-
-    recursive(permus, "", given_word)
-
-    print("All Permutations generated: ", len(permus))
-
-    return permus
-
-
 def checkvalidconundrum(wordin):
 
     # Used the check if the 9 letter word is valid
@@ -55,35 +27,42 @@ def checkvalidconundrum(wordin):
     number_of_consonants = sum(wordin.count(c) for c in consonants)
     number_of_vowels = sum(wordin.count(c) for c in vowels)
 
-    wordin = wordin.strip()
+    wordin = wordin.strip()  # Strips the whitespace from both sides of the word
 
-    if len(wordin) != 9:
+    if len(wordin) != 9:  # If the anagram given is 9 letters long
         return False
-    if number_of_consonants < 4:
+    if number_of_consonants < 4:  # If the anagram contains at least 4 consonants
         return False
-    if number_of_vowels < 3:
+    if number_of_vowels < 3:  # If the anagram contains at least 3 vowels
         return False
-    if wordin.isalpha() is False:
+    if wordin.isalpha() is False:  # If all the letters are just alphabetical
         return False
+
+    # If all the above checks are passed we have a valid possible conundrum
 
     return True
 
 
 def dropwords(anagram, wordlist):
 
-    # Checks if a word contains the same letters as the anagram keep it
-    # If it contains a letter that doesn't exist in the anagram, drop the word
-    # Example would be MUG - CONUNDRUM as sets is..
-    # MUG - CONDRUM
-    # G is left over because CONUNDRUM doesn't contain that letter
-    # Drop the word because you can't make an anagram with it
+    # Takes in the possible anagram for example PINESSHAP
+    # Takes in the list of dictionary words list
 
-    cleanwordlist = set()
-    letters = set(anagram)
+    # Next the anagram is parsed to a set e.g PINESSHAP to PINESHA, all duplicate letters are discarded
+    # Then for each word in the words list you parse that to a set and subtract both sets from each other
+    # e.g the word CAN - PINESHA leaves a left over of the letter C which is a total of 1 leftover letter (we look for none)
+    # The letter C cannot be used to create an anagram this means we can discard the word Can and not add it to the new word list
+    # An example of a word that would be added to the set is NIP - PINESHA leaves no left over letters (0)
 
-    for word in wordlist:
-        if len(list(set(word) - set(letters))) is 0:
-            cleanwordlist.add(word)
+    cleanwordlist = set()  # Set of words that meet the criteria
+    letters = set(anagram)  # Creates a set of letters of the given anagram
+
+    for word in wordlist: # For each word in the wordlist
+        if len(list(set(word) - set(letters))) is 0:  # If the leftover set contains no letters
+            cleanwordlist.add(word)     # Add the word to the list of words matching the criteria
+
+    # Finally return the wordlist of possible words
+
     return cleanwordlist
 
 
@@ -92,15 +71,15 @@ def user_input():
     print("Running solver.py")
     print("--------------")
 
-    anagram = input("Please enter a valid conundrum? ")
+    anagram = input("Please enter a valid conundrum? ")     # Takes the user input
 
-    while checkvalidconundrum(anagram) is False:
+    while checkvalidconundrum(anagram) is False:    # Checks whether the user word is valid if not, try again
         print("\nThe conundrum didn't meet the criteria, try again")
         anagram = input("Please enter a valid conundrum? ")
 
-    anagram = anagram.upper()
+    anagram = anagram.upper()   # Converts the word to uppercase
 
-    print("Valid Anagram: ", anagram)
+    print("Valid Anagram: ", anagram)       # Print success message and return the valid, uppercase word
 
     return anagram
 
@@ -110,28 +89,30 @@ def random_input():
     print("Running solver.py")
     print("--------------")
 
-    vowels = list("AEIOU")
-    consonants = list("BCDFGHJKLMNPQRSTVWXYZ")
+    vowels = list("AEIOU")      # List of vowels
+    consonants = list("BCDFGHJKLMNPQRSTVWXYZ")      # List of consonants
 
     count = 0
 
     word = ""
 
-    word += (random.choice(consonants))
-    word += (random.choice(consonants))
-    word += (random.choice(consonants))
-    word += (random.choice(consonants))
+    word += (random.choice(consonants))     # Added the first random consonant
+    word += (random.choice(consonants))     # Added the second random consonant
+    word += (random.choice(consonants))     # Added the third random consonant
+    word += (random.choice(consonants))     # Added the fourth random consonant
 
-    while count < word.count('Q'):
-        word += 'U'
-        count += 1
+    while count < word.count('Q'):      # Checks if the word has any Q's if so...
+        word += 'U'     # Add a corresponding U
+        count += 1      # Mark that we added a vowel (U)
 
-    while count < 3:
-        word += (random.choice(vowels))
-        count += 1
+    while count < 3:    # While count of vowels is less then 3 (could have increased because of the Q's and added U's)
+        word += (random.choice(vowels))     # Add a random vowel
+        count += 1      # Mark that a vowel was added
 
-    secondlast = random.randint(0, 1)
-    last = random.randint(0, 1)
+    # Now 7/9 letters have been added we randomly choose the last 2 letters to add
+
+    secondlast = random.randint(0, 1)       # Random integer between 0 and 1, 0 for a vowel or 1 for a consonant
+    last = random.randint(0, 1)     # Random integer between 0 and 1, 0 for a vowel or 1 for a consonant
 
     if last == 0:
         word += (random.choice(vowels))
@@ -143,6 +124,8 @@ def random_input():
     else:
         word += (random.choice(consonants))
 
+    # Return the finished 9 letter word meeting the criteria
+
     return word
 
 
@@ -152,6 +135,8 @@ def reset():
     global dictionary
     global anagram
 
+    # Reset the globals for running the program again
+
     debug_mode = True
     anagramFound = False
     dictionary = set()
@@ -159,6 +144,9 @@ def reset():
 
 
 def main_program():
+
+    # Calls the main program method
+
     algorithm()
 
 
@@ -167,10 +155,10 @@ def preprocessing(anagram):
 
     print("Random Anagram: ", " ".join(anagram))
 
-    dictionary = preproc.import_dictionaryfile("wordfile.txt") # Importing the dictionary file
-    dictionary = dropwords(anagram, dictionary)
+    dictionary = preproc.import_dictionaryfile("wordfile.txt")  # Importing the dictionary file
+    dictionary = dropwords(anagram, dictionary)     # See the dropwords method
 
-    dictionary = sorted(dictionary, key=len, reverse=True)
+    dictionary = sorted(dictionary, key=len, reverse=True)      # Orders the dictionary by length descending
 
     return dictionary
 
@@ -179,29 +167,45 @@ def algorithm():
     global dictionary
     global anagram
 
-    size = 9  # ideal size of conundrum
+    wordfound = "No word found"     # Default word
     count = 0
-    wordfound = "No word found"
 
     t0 = time.time()
 
-    while size > 2:
-        for word in dictionary:
+    # NB at this point we know the dictionary is in descending order my length
 
-            count += 1
+    for word in dictionary:     # For each word in the wordlist dictionary
 
-            if collections.Counter(word) == collections.Counter(anagram):
-                print("\nFound a nine letter conundrum!")
-                wordfound = word
-                size = 0
-                break
-        size -= 1
+        count += 1      # Increase the count, just for tracking the amount of iterations
+
+        if collections.Counter(word) == collections.Counter(anagram):
+
+            # If the word contains the same amount of matching letters as the anagram
+
+            print("\nFound a nine letter conundrum!")
+            wordfound = word
+            break
+
+        if len(word) != 9:  # Once you reach
+            break
 
     # Above gets 9 letter conundrum
     # -----------------------------
 
     for word in dictionary:
+
+        count += 1
+
         if not collections.Counter(word) - collections.Counter(anagram):
+
+            # Removes the anagram letters from the word
+            # If any letters are left over in the collections.Counter(word) it's not an anagram and returns true because there is information in the counter
+            # Otherwise the counter is empty which means we have a valid anagram
+
+            print()
+            print("collections.Counter(word): ", collections.Counter(word))
+            print("collections.Counter(anagram): ", collections.Counter(anagram))
+            print("collections.Counter(word) - collections.Counter(anagram): ", collections.Counter(word) - collections.Counter(anagram))
             wordfound = word
             break
 
@@ -211,7 +215,7 @@ def algorithm():
 
     print("\nTime: ", time.time() - t0)
 
-    print("\nCount: ", count, size)
+    print("\nCount: ", count)
 
 
 # Main Program
@@ -222,7 +226,7 @@ anagramFound = False
 dictionary = set()
 anagram = ""
 
-# Variables
+# Input
 
 anagram = random_input()
 #anagram = user_input()
